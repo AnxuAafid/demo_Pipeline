440cb856a09a3a6aaa9579ffccd6de8569aa19b1
Anzar Hussain <anzarhussain@Anzars-MacBook-Air.local>
2025-06-03 11:20:03 +0530

    all files added

---
+++ census_income/utils/util.py
@@ -0,0 +1,1 @@
+# here we keeep the functions which are common every where
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(name="census_income",
+      version="0.0.1",
+      author="anxie",
+      author_email="anxuaafid15@gmail.com",
+      packages=find_packages(),
+      install_requires = ["pandas", "numpy", "flask"])
